09d361f636ecfaa2a736528d1109fb61f5b3994c
Ashutosh Maurya <ashutoshm9695@gmail.com>
2023-01-13 23:55:49 +0530

    initial setup

---
+++ src/homepage/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <div className="left"></div>
+      <div className="center"></div>
+      <div className="right"></div>
+    </div>
+  );
+};
+
+export default Homepage;
